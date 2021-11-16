6f4c090e941ea0fe082513bff650a27860b90eea
Roie Schwaber-Cohen <roie.cohen@gmail.com>
2021-11-16 10:29:08 -0800

    identity

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,62 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import { useEffect, useState } from 'react'
+import netlifyAuth from '../netlifyAuth.js'
+
 
 export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated)
+    let [user, setUser] = useState(null)
 
-      <main>
-        <Header title="Welcome to my app!" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-      </main>
+    let login = () => {
+        netlifyAuth.authenticate((user) => {
+            setLoggedIn(!!user)
+            setUser(user)
+            netlifyAuth.closeModal()
+        })
+    }
 
-      <Footer />
-    </div>
-  )
+    let logout = () => {
+        netlifyAuth.signout(() => {
+            setLoggedIn(false)
+            setUser(null)
+        })
+    }
+
+    useEffect(() => {
+        netlifyAuth.initialize((user) => {
+            setLoggedIn(!!user)
+        })
+    }, [loggedIn])
+
+    return (
+        <div className="container">
+            <Head>
+                <title>Next.js Starter!</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <main>
+                <Header title="Welcome to my app!" />
+                <p className="description">
+                    {loggedIn ? (
+                        <div>
+                            You are logged in!
+                            {user && <>Welcome {user?.user_metadata.full_name}!</>}
+                            <button onClick={logout}>
+                                Logout
+                            </button>
+                        </div>
+                    ) : (
+                        <button onClick={login}>
+                            Log in here.
+                        </button>
+                    )}
+                </p>
+            </main>
+
+            <Footer />
+        </div>
+    )
 }
